fde276f1d1adbd455b808a0fb87994437257ce34
sage-ali <81039794+sage-ali@users.noreply.github.com>
2026-04-18 00:02:19 +0100

    chore: clean up directory

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   base: '/FE-stage-1/',
-})
+});
